6e67b9706a7d12612c0fb72d4d92ec99ee63a5a1
William Robert Simpson <will.simpson85@gmail.com>
2019-01-22 22:22:55 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  newArray = []
+ if block_given?
+     array.each do |item|
+      if yield(item)
+        newArray << item
+      end
+     end
+   newArray
+   end
+
 end
