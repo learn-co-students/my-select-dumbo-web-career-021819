923491e84f2af2ffbd8fdd20f6e66aeed39464a0
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-01 05:55:46 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  new_collection = []
+  i = 0
+  while i < collection.size
+    element = collection[i]
+    new_collection << element if yield(element) == true
+    i += 1
+  end
+  new_collection
 end
+
+
+
