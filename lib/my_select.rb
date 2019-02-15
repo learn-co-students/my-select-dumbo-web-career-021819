f8539ab8b1ba7adee08aacb0d1d7954995a1d505
Ashley Archibald <aarchibald20@gmail.com>
2019-02-15 06:25:37 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+ count = 0
+ array = []
+
+ while count < collection.size
+   if yield(collection[count]) == true
+     array.push(collection[count])
+ end
+    count += 1
+end
+array
 end
